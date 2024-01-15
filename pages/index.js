4ce277a0ea5c1e5ba412b4e464acf8e9b030b689
vittal <vitthalsshindhe2001@gmail.com>
2024-01-15 11:50:59 +0530

    Next js Application

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,132 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+export default function index() {
+  const [expression, setExpression] = useState('');
+  const [expressionHistory, setExpressionHistory] = useState([])
+  console.table(expressionHistory)
+  console.log(expression);
+  const [result, setResult] = useState('');
+  console.log(result);
 
-export default function Home() {
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        if (expression.length > 0) {
+          const calculatedResult = eval(expression);
+          setExpressionHistory(expressionHistory => [...expressionHistory, `${expression}=${calculatedResult}`]);
+          setResult(calculatedResult);
+          setExpression(calculatedResult.toString());
+        }
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setExpression('');
+      setResult('');
+    } else if (value === '⌫') {
+      setExpression(expression.slice(0, -1));
+    } 
+    else {
+      setExpression((prevExpression) => prevExpression + value);
+    }
+  };
+
+  const handleOperationClick = (operator) => {
+    const lastCharIsOperator = /[+\-*/]$/.test(expression);
+
+    if (lastCharIsOperator) {
+      setExpression((prevExpression) =>
+        prevExpression.slice(0, -1) + operator
+      );
+    } else {
+      setExpression((prevExpression) => prevExpression + operator);
+    }
+  };
+
+  const handleNumberClick = (number) => {
+    handleButtonClick(number);
+  };
+
+  const clearHistory = () => {
+    setExpressionHistory([])
+    setResult("")
+    setExpression("")
+  }
+  const printHistory = () => {
+    if (typeof window !== 'undefined') {
+      const content = expressionHistory.map(calculation => `<p>${calculation}</p>`).join('');
+      const pageSizeStyles = `
+        @page {
+          size: 80mm  80mm;
+          /* Adjust the width and height values based on your custom page size */
+        }
+      `;
+      
+      const printableBlob = new Blob([`<html><head><title>Bill Invoice</title><style>${pageSizeStyles}</style></head><body>${content}</body></html>`], { type: 'text/html' });
+      const printableUrl = URL.createObjectURL(printableBlob);
+  
+      const printWindow = window.open(printableUrl, '_blank');
+      
+      if (printWindow) {
+        printWindow.onload = () => {
+          printWindow.print();
+          printWindow.onafterprint = () => {
+            printWindow.close();
+            URL.revokeObjectURL(printableUrl); // Release the object URL
+          };
+        };
+      }
+    }
+  };
+  
+  
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="main">
+      <div className="display">
+        <button className="dark_mode_btn">
+          <i className="fa fa-moon-o" aria-hidden="true"></i>
+        </button>
+        <h2 id="history">{!handleOperationClick && result > 0 ? result : expression}</h2>
+        <div className="mt-2 ml-5">
+          {/* <!-- <button class="btn btn-success" onclick="saveHistory()">Save History</button>--> */}
+          <button className="btn btn-info " onClick={clearHistory}>Clear History</button><span></span><span></span>
+          {/* <!-- <button class="btn btn-warning" onclick="calculateTotalBill()">Total Bill</button> --> */}
+          <button className="btn btn-primary ml-4" onClick={printHistory}>Print History</button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="buttons">
+        <button id="clear" onClick={() => handleButtonClick('C')}>C</button>
+        <button id="delete_single_num" onClick={() => handleButtonClick('⌫')}><i className="fa fa-scissors" aria-hidden="true"></i></button> <button id="Normal_btn" onClick={() => handleOperationClick('/')}>/</button>
+        <button id="Normal_btn" onClick={() => handleOperationClick('*')}>*</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="buttons">
+        <button id="Normal_btn" onClick={() => handleNumberClick('7')}>7</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('8')}>8</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('9')}>9</button>
+        <button id="Normal_btn" onClick={() => handleOperationClick('-')}>-</button>
       </div>
-    </main>
+      <div className="buttons">
+        <button id="Normal_btn" onClick={() => handleNumberClick('4')}>4</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('5')}>5</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('6')}>6</button>
+        <button id="Normal_btn" onClick={() => handleOperationClick('+')}>+</button>
+      </div>
+      <div className="buttons">
+        <button id="Normal_btn" onClick={() => handleNumberClick('1')}>1</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('2')}>2</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('3')}>3</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('.')}>.</button>
+      </div>
+      <div className="buttons">
+        <button id="Normal_btn" onClick={() => handleNumberClick('0')}>0</button>
+        <button id="Normal_btn" onClick={() => handleNumberClick('00')}>00</button>
+        <button id="equalTo" onClick={() => handleButtonClick('=')}>=</button>
+
+      </div>
+    </div>
   )
 }
+
+
+
